45df976258ed960e074d34a7d8efe55ae1e3b426
ansarjarvis <ansar.ofcl00@gmail.com>
2021-08-05 13:44:03 +0530

    seeds cloud database

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const Campground = require("../model/compground");
 const cities = require("./city");
 const { places, descriptors } = require("./seed-helper");
 
-mongoose.connect('mongodb://localhost:27017/camp-ground', { useNewUrlParser: true, useUnifiedTopology: true });
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/camp-ground';
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
