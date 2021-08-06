56352c4ded3cfbc0818333f8eb886300ed83eb91
ansarjarvis <ansar.ofcl00@gmail.com>
2021-08-07 00:24:23 +0530

    fix some bugs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require("helmet");
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/camp-ground' || process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/camp-ground';
 mongoose.connect(dbUrl, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
